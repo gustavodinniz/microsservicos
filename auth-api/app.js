021f999520266689d0715d47cb1f43c1257a8794
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-01 09:38:14 -0300

    autenticação jwt, middleware de autenticação e impletação de segurança nos endpoints.

--- auth-api/app.js
+++ auth-api/app.js
@@ -8,9 +8,6 @@ const PORT = env.PORT || 8083;
 
 db.createInitialData();
 
-app.use(express.json());
-app.use(userRoutes);
-
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: 'Auth-API',
@@ -18,6 +15,9 @@ app.get('/api/status', (req, res) => {
         httpStatus: 200,
     });
 })
+
+app.use(express.json());
+app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.info(`---> Server started successfully at port ${PORT}`);
--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,15 +1,24 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import UserRepository from "../repository/UserRepository.js";
-import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
+import * as httpStatus from "../../../config/constants/httpStatus.js";
+import * as secrets from "../../../config/constants/secrets.js"
+
 
 class UserService {
 
     async findByEmail(req) {
         try {
+            
             const { email } = req.params;
+            const { authUser } = req;
+
             this.validateRequestData(email);
             let user = await UserRepository.findByEmail(email);
             this.validateUserNotFound(user);
+            this.validateAuthenticatedUser(user, authUser);
             console.info("---> Fetching User by Email");
             return {
                 status: httpStatus.SUCCESS,
@@ -42,6 +51,52 @@ class UserService {
                 httpStatus.BAD_REQUEST,
                 "User was not found."
             );
+        }
+    }
+
+    validateAuthenticatedUser(user, authUser) {
+        if (!authUser || (user.id !== authUser.id)) {
+            throw new UserException(
+                httpStatus.FORBIDDEN,
+                "You cannot see this user data."
+            );
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validateAccessTokenData(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = { id: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET, { expiresIn: '1d' });
+            return {
+                status: httpStatus.SUCCESS,
+                accessToken,
+            }
+        } catch (err) {
+            return {
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: err.message,
+            };
+        }
+
+    }
+
+    validateAccessTokenData(email, password) {
+        if (!email || !password) {
+            throw new UserException(
+                httpStatus.UNAUTHORIZED,
+                "Email and password must be informed."
+            );
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password doesn't match.")
         }
     }
 
