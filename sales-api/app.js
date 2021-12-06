a79a6b164637bd16a5dfc5bf87a229840edbf8d8
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-06 19:42:02 -0300

    criando models do MongoDB e inserindo dados iniciais

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,20 +1,22 @@
-import express  from "express";
+import express from "express";
 
 import { connect } from "./src/config/db/mongoDbConfig.js";
+import { createInitialData } from "./src/config/db/initialData.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
 connect();
+createInitialData();
 
-app.get('/api/status', (req, res) => {
+app.get('/api/status', async (req, res) => {
     return res.status(200).json({
         service: "Sales-API",
         status: "up",
         httpStatus: 200,
-    })
-})
+    });
+});
 
 app.listen(PORT, () => {
     console.info(`Server started successfully at port ${PORT}`);
---
+++ sales-api/src/config/db/initialData.js
@@ -0,0 +1,52 @@
+import Order from "../../modules/sales/model/Order.js";
+
+export async function createInitialData() {
+    await Order.collection.drop();
+    await Order.create({
+        products: [
+            {
+                productId: 1001,
+                quantity: 1
+            },
+            {
+                productId: 1002,
+                quantity: 2
+            },
+            {
+                productId: 1003,
+                quantity: 2
+            },
+        ],
+        user: {
+            id: 'af7s7af87efy89a',
+            name: 'Gustavo',
+            email: 'gustavodinniz@hotmail.com'
+        },
+        status: 'APPROVED',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+    await Order.create({
+        products: [
+            {
+                productId: 1001,
+                quantity: 3
+            },
+            {
+                productId: 1003,
+                quantity: 1
+            },
+        ],
+        user: {
+            id: 'ug46yi43g6ui34',
+            name: 'Alberto',
+            email: 'alberto@hotmail.com'
+        },
+        status: 'REJECTED',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+
+    let initialData = await Order.find();
+    console.info(`Initial data was created: ${JSON.stringify(initialData)}`);
+}
