b3464f8ecd8f2c25fbc09e3edd9356cdf86257f6
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-06 21:27:52 -0300

    criando listener e sender da aplicacao

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,17 +1,44 @@
 import express from "express";
 
-import { connect } from "./src/config/db/mongoDbConfig.js";
+import { connectMongoDb } from "./src/config/db/mongoDbConfig.js";
 import { createInitialData } from "./src/config/db/initialData.js";
 import checkToken from "./src/config/auth/checkToken.js";
+import { connectRabbitMq } from "./src/config/rabbitmq/rabbitConfig.js"
+
+import { sendMessageToProductStockUpdateQueue } from "./src/modules/product/rabbitmq/productStockUpdateSender.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-connect();
+connectMongoDb();
 createInitialData();
+connectRabbitMq();
 
-app.use(checkToken);
+// app.use(checkToken);
+
+app.get('/test', (req, res) => {
+    try {
+        sendMessageToProductStockUpdateQueue([
+            {
+                productId: 1001,
+                quantity: 3,
+            },
+            {
+                productId: 1002,
+                quantity: 2,
+            },
+            {
+                productId: 1003,
+                quantity: 1,
+            },
+        ])
+        return res.status(200).json({ error: 200 });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: trues });
+    }
+});
 
 app.get('/api/status', async (req, res) => {
     return res.status(200).json({
--- sales-api/src/config/secrets/secrets.js
+++
@@ -1,9 +0,0 @@
-const env = process.env;
-
-export const MONGO_DB_URL = env.MONGO_DB_URL
-    ? env.MONGO_DB_URL
-    : "mongodb://admin:123456@localhost:27017/sales";
-
-export const API_SECRET = env.API_SECRET
-    ? env.API_SECRET
-    : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=";
